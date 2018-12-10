c8ebddaf1389123eeb452e0ea0a1594b231ef939
jennyyao98 <35282159+jennyyao98@users.noreply.github.com>
2018-12-09 22:00:06 -0500

    Update roomcreated2.php

--- room creation/roomcreated2.php
+++ room creation/roomcreated2.php
@@ -1,3 +1,4 @@
+<!-- Jenny Yao, ROCA1 -->
 <!-- Room Creation Upload -->
 
 <!-- Fix move_uploaded_file on line 44
@@ -20,10 +21,10 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  <!-- <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet"> -->
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="stylesheet.css">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="fonts/stylesheet"> -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="style.css"> -->
+  <link rel="stylesheet" href="fonts/stylesheet.css">
 
   <!-- <link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
   <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
@@ -31,36 +32,103 @@
   <title>Room Creation for <?php echo $_POST["Room"]; ?></title>
 </head>
 <body>
-  <!-- <script>
-  function myFunction{
-    $('#go').click(function(){
-      $('#shape').css('width',$('#w').val());
-      $('#shape').css('height',$('#h').val());
-      $('#shape').css('border','2px solid black');
-    });
-});
-  </script> -->
-  <!-- <style>
-    .wrapper{
-      position: relative;
-      top: 0;
-      left: 0;
-    }
-    .image1 {
-  position: relative;
-  top: 0;
-  left: 0;
-    }
-    .image2 {
-      position: absolute;
-      top: 30px;
-      left: 70px;
-    }
-  </style> -->
-<h1><?php echo $_POST["Room"]; ?></h1>
-<br>
-<h4 style="padding-bottom: 2%;">Seating Capacity: <?php echo $_POST["Size"]; ?></h4>
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
 
+  *{
+    font-family: 'product sans';
+    margin: 0;
+    background-color: #f2f2f2;
+
+  }
+  body{
+    padding: 2%;
+    padding-left: 7%;
+    padding-right: 7%;
+  }
+  h1{
+    padding-bottom: 0;
+  }
+  h2{
+    padding-top: 2%;
+  }
+  h3{
+    padding-bottom: 3%;
+    padding-top: 2%;
+  }
+  .description{
+    font-size: 100%;
+    background-color: transparent;
+    color: black;
+  }
+  p{
+    font-size: 50%;
+    padding-bottom: 2%;
+    background-color: #000000;
+    color: #ffffff
+  }
+
+  input[type=text] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 2px solid #000000 ;
+      border-radius: 20px;
+      box-sizing: border-box;
+      color:#9B9B9B;
+      background-color: #ffffff;
+  }
+
+  input[type=submit] {
+      width: 70%;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: 2px solid #ffffff ;
+      border-radius: 20px;
+      box-sizing: border-box;
+      border-radius: 20px;
+      background-color: #000000;
+      color: #ffffff;
+      cursor: pointer;
+  }
+  input[type=submit]:hover {
+    width: 70%;
+    padding: 14px 20px;
+    margin: 8px 0;
+    color:#000000;
+    background-color: #ffffff;
+  }
+  #finalSubmit{
+    margin-left: 15%;
+  }
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    padding: 2%;
+  }
+  .grid-item {
+    border: 2px solid #ccc;
+    border-radius: 6%;
+    background-color: black;
+    padding: 20px;
+    font-size: 30px;
+    text-align: center;
+  }
+  .wrapper{
+  position:relative;
+  }
+  .absolute_pic{
+    position: absolute;
+  }
+
+  </style>
+
+ <h1><strong><?php echo $_POST['Room']; ?></strong></h1>
+<h4 style="padding-bottom: 2%;">Seating Capacity: <?php echo $_POST['Size']; ?></h4>
+
+<!-- Checks image submission & uploads -->
 <?php
 if (isset($_POST["submit"])) {
   $file = $_FILES['file'];
@@ -81,14 +149,8 @@ if (isset($_POST["submit"])) {
   if (in_array($fileActualExt,$allowed)) {
     if ($fileError === 0) {
       if($fileSize < 9600000){ //less than 12.5 mb
-          // $fileNameNew = uniqid('',true).".".$fileActualExt;
-          // $fileDestination = '../upload'.$fileNameNew;
-          // move_uploaded_file($fileTempName, $fileDestination); //parameter: temp location parameter 2: new location
-          // // header("Location: index.php?uploadsuccess")
-          // echo $fileNameNew;
-          // print_r(move_uploaded_file($fileName, $fileDestination));
           if (move_uploaded_file($fileTempName, $target_file)) { //error on move_uploaded_file
-              echo "Your file: ". basename($fileName). " has been uploaded.";
+              echo "<i>Image upload success.</i>";
 
           } else {
               echo '<span style="color:#ff0000;">Sorry, there was an error uploading your file.</span>';
@@ -112,56 +174,113 @@ if (isset($_POST["submit"])) {
   }
 }
 ?>
-<h3>Coordinates</h3>
+<h3><strong>Setting the Sections of the Classroom</strong></h3>
 
-<!-- Diplay grid over uploaded img [Problem: the imgs are not chnaging] -->
-<div class="wrapper" style="position: relative; padding-bottom:2%">
-  <img src="GRID.png" alt="grid" style="max-width:100%; max-height:100%; position: absolute;">
-  <!-- <div class="upload-pic">
+	
+	
+	
+<!-- Diplay grid over uploaded img  -->
+<!--
+Bottom Left:  (x1,y1)
+  x1 = Used to sets Left margin
+  y1
+Top Right:    (x2,y2)
+  x2
+  y2 = Used to set Top margin
+ -->
 
-  </div> -->
-  <img src="upload/pic.jpg" alt="your image" style="max-width:70%; max-height: 60%;">
+<!--
+Dimensions of 1 box: BL(0,0); TR(1,1)
+  height: 5.5%;
+  width: 3.4%;
+-->
+
+ <!--
+ To determine the width and height of the boxes:
+ - width= (x2-x1) * 3.4%
+ - height= (y2-y1) * 5.5%
+-->
+
+<!--
+To determine the position of the boxes (margin-top,left):
+- initial margins:
+    - intial margin-left = 4.7%
+    - initial margin-top = 1.2%
+- margin-left= (x1 * 3.4%) + 4.7%
+- margin-top= ((12-y2) * 5.5%) + 1.2%
+-->
+
+<div class="wrapper" style="height: 100vh;">
+<!-- currently for BL(0,11)    TR(1,12) -->
+<!-- currently: make sure the porportion of the width to height is always the same-->
+  <div
+  style="
+  /* position: fixed; */
+  /* OK */
+  height:5.5%;
+  /* OK */
+  width: 3.4%;
+  max-height: 100%;
+  z-index:4;
+  /* OK */
+  margin-left: 4.7%;
+  margin-top: 1.2%;
+  /* margin: 16.3% 0 0 31.9%; */
+  position: absolute;
+  border: 2px solid #000000 ;
+  border-radius: 2px;
+  box-sizing: border-box;
+  background-color: rgba(98,86, 80, 0.3)	/* 50% opaque blue */
+;
+  /* box-sizing: box-shadow: 10px 10px 28px 1px rgba(163,163,163,0.54); */
+
+  "
+  >
+</div>
+	
+	
+	
+	
+  <div style="height:100%; z-index:3;position: absolute;">
+  <img src="grid.png" alt="grid" style="width: 100%; max-height:100%;   background-color: transparent;
+">
+</div>
+<div style="height:100%;  z-index:2;position: absolute;">
+  <img class ="image2" src="upload/pic.jpg" alt="your image" style="max-width:90%; height:100%; margin-left:5%;">
+</div>
 </div>
 
-<h5>Label the Top left, Top right, and Bottom Right coordinates of the each box to create the designated dimensions.</h5>
+<p class="description"><strong>Label the Bottom left and Top right coordinates of the boxes to create the designated dimensions. The 6 box does not all have to be used.<br>
+(The Box numbers displayed on the image can be used as a guidance but does not have to be followed strictly.)</strong></p>
 <form action="roomCreationSuccess.php" method="post">
-
+<input type="hidden" name="action" value="part2">
+<input type="hidden" name="Room" value="<?php echo $_POST['Room'];?>">
+<input type="hidden" name="Size" value="<?php echo $_POST['Size'];?>">
+<input type="hidden" name="file" value="upload/pic.jpg">
 <div class="grid-container">
   <div class="grid-item">
     <p>Box 1</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-<?php
-	if ($_GET["coord"]==1) {
-		echo 'value= "'.$_GET["0"].'"';
-	} else {
-		echo 'placeholder= "Wid"';
-	}
-?>
-           />
+           name="data[]"
+           <?php
+           if (isset($_POST['data'])) {
+             echo 'value="'.$_POST["data"][0].'"';
+          } else {
+
+          echo '  placeholder= "Bottom Left: (  ,  )"';
+          }
+
+             ?>
+
+
+
+    />
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-<?php
-	if ($_GET["coord"]==1) {
-		echo 'value= "'.$_GET["1"].'"';
-	} else {
-		echo 'placeholder = "Top Right: (  ,  )"';
-	}
-?>
-            />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-<?php
-	if ($_GET["coord"]==1) {
-		echo 'value= "'.$_GET["1"].'"';
-	} else {
-		echo 'placeholder = "Bottom Right: (  ,  )"';
-	}
-?>
-           />
+           name="data[]"
+
+           placeholder = "Top Right: (  ,  )" />
     <input type="submit" name="Box1" value="Preview">
 
   </div>
@@ -169,48 +288,30 @@ if (isset($_POST["submit"])) {
     <p>Box 2</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-           placeholder = "Top Left: (  ,  )"/>
+           placeholder = "Bottom Left: (  ,  )"/>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
            placeholder = "Top Right: (  ,  )" />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-           placeholder = "Bottom Right: (  ,  )" />
     <input type="submit" name="Box2" value="Preview">
   </div>
   <div class="grid-item">
     <p>Box 3</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-           placeholder = "Top Left: (  ,  )"/>
+           placeholder = "Bottom Left: (  ,  )"/>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
            placeholder = "Top Right: (  ,  )" />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-           placeholder = "Bottom Right: (  ,  )" />
     <input type="submit" name="Box3" value="Preview">
   </div>
   <div class="grid-item">
     <p>Box 4</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-           placeholder = "Top Left: (  ,  )"/>
+           placeholder = "Bottom Left: (  ,  )"/>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
            placeholder = "Top Right: (  ,  )" />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-           placeholder = "Bottom Right: (  ,  )" />
     <input type="submit" name="Box4" value="Preview">
 
   </div>
@@ -218,39 +319,26 @@ if (isset($_POST["submit"])) {
     <p>Box 5</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-           placeholder = "Top Left: (  ,  )"/>
+           placeholder = "Bottom Left: (  ,  )"/>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
            placeholder = "Top Right: (  ,  )" />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-           placeholder = "Bottom Right: (  ,  )" />
   <input type="submit" name="Box5" value="Preview">
   </div>
   <div class="grid-item">
     <p>Box 6</p>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
-           placeholder = "Top Left: (  ,  )"/>
+           placeholder = "Bottom Left: (  ,  )"/>
     <input type = "text"
            id = "myText"
-           name = "coord[]"
            placeholder = "Top Right: (  ,  )" />
-    <input type = "text"
-           id = "myText"
-           name = "coord[]"
-           placeholder = "Bottom Right: (  ,  )" />
     <input type="submit" name="Box6" value="Preview">
   </div>
 </div>
-<input type="submit" name="submit" value="Create Room">
+<input id="finalSubmit" type="submit" name="submit" value="Create Room">
 
 </form>
-
 </body>
 </html>
 
